9550caec0b999b03eb326a225be5155009261a7e
krabecb <60526718+krabecb@users.noreply.github.com>
2020-09-27 20:18:32 -0500

    Added app ID and key

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 function App() {
 
   const APP_ID = '32887e3f'
+  const APP_key = 'a3682fcc7c9c1dbbf3f353117ade4f36'
+
+  
 
   return (
     <div className="App">
-      <h1>Hello React </h1>
+      <h1>Hello React</h1>
     </div>
   );
 }
