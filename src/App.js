173ba3af361c6753ce3055efcacf1d26d230302b
krabecb <60526718+krabecb@users.noreply.github.com>
2020-09-28 22:14:47 -0500

    getRecipes returns data

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ const App = () => {
   //Normally you'd use environment tools to protect these.
   const APP_ID = '32887e3f'
   const APP_KEY = 'a3682fcc7c9c1dbbf3f353117ade4f36'
-  const exampleText = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
-
-  const [counter, setCounter] = useState(0)
 
   useEffect(() => {
-  	console.log('Effect has been run')
-  })
+  	getRecipes()
+  }, []) //Second argument of useEffect. Empy array makes useEffect run once and not everytime we increment the counter. [counter]
+  				//makes useEffect run everytime counter changes.
+
+  const getRecipes = async () => {
+  	const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`) //We're waiting and requesting from a external API
+  																												//We don't know when this info will come back. So
+  																												//we need to add await(line 18) every time we have
+  																												//a promise.
+  	const data = await response.json() //Formats in a way that we can work with the data.
+  	console.log(data)
+  }
 
   return (
     <div className="App">
@@ -22,7 +29,6 @@ const App = () => {
       		Search
       	</button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
