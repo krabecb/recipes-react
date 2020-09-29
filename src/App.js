d857d38cb0a4786cb0fa62c3ffdd555c297c7512
krabecb <60526718+krabecb@users.noreply.github.com>
2020-09-28 21:09:14 -0500

    Imported useEffect and useState. Created h1 with an onClick to increment counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
 
+  //Normally you'd use environment tools to protect these.
   const APP_ID = '32887e3f'
-  const APP_key = 'a3682fcc7c9c1dbbf3f353117ade4f36'
+  const APP_KEY = 'a3682fcc7c9c1dbbf3f353117ade4f36'
+  const exampleText = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-  
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+  	console.log('Effect has been run')
+  })
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+      	<input className="search-bar" type="text"/>
+      	<button className="search-button" type="submit">
+      		Search
+      	</button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
